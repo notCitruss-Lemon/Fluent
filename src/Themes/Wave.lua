e27c4050e5db01a35727f4c1a7ea9baecd51dff7
c4lxne <166979659+c4lxne@users.noreply.github.com>
2024-04-24 19:08:10 +0700

    Add files via upload

---
+++ src/Themes/Wave.lua
@@ -0,0 +1,47 @@
+return {
+	Name = "Wave",
+	Accent = Color3.fromRGB(151, 255, 255),
+
+	AcrylicMain = Color3.fromRGB(198, 266, 255),
+	AcrylicBorder = Color3.fromRGB(141, 288, 288),
+	AcrylicGradient = ColorSequence.new(Color3.fromRGB(60, 140, 140), Color3.fromRGB(40, 80, 80)),
+	AcrylicNoise = 0.92,
+
+	TitleBarLine = Color3.fromRGB(141, 238, 238),
+	Tab = Color3.fromRGB(121, 205, 205),
+
+	Element = Color3.fromRGB(224, 238, 238),
+	ElementBorder = Color3.fromRGB(193, 205, 205),
+	InElementBorder = Color3.fromRGB(135, 206, 235),
+	ElementTransparency = 0.84,
+
+	ToggleSlider = Color3.fromRGB(110, 160, 160),
+	ToggleToggled = Color3.fromRGB(0, 154, 205),
+
+	SliderRail = Color3.fromRGB(110, 160, 160),
+
+	DropdownFrame = Color3.fromRGB(126, 192, 238),
+	DropdownHolder = Color3.fromRGB(0, 191, 255),
+	DropdownBorder = Color3.fromRGB(40, 65, 65),
+	DropdownOption = Color3.fromRGB(110, 160, 160),
+
+	Keybind = Color3.fromRGB(110, 160, 160),
+
+	Input = Color3.fromRGB(110, 160, 160),
+	InputFocused = Color3.fromRGB(20, 10, 30),
+	InputIndicator = Color3.fromRGB(130, 170, 170),
+
+	Dialog = Color3.fromRGB(126, 192, 138),
+	DialogHolder = Color3.fromRGB(30, 60, 60),
+	DialogHolderLine = Color3.fromRGB(25, 50, 50),
+	DialogButton = Color3.fromRGB(240, 255, 255),
+	DialogButtonBorder = Color3.fromRGB(80, 110, 110),
+	DialogBorder = Color3.fromRGB(191, 239, 255),
+	DialogInput = Color3.fromRGB(45, 90, 90),
+	DialogInputLine = Color3.fromRGB(130, 170, 170),
+
+	Text = Color3.fromRGB(240, 240, 240),
+	SubText = Color3.fromRGB(170, 170, 170),
+	Hover = Color3.fromRGB(110, 160, 160),
+	HoverChange = 0.04,
+}
