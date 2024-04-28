95607405bfb5f0b0da70cfa87743e663d8f810ea
c4lxne <166979659+c4lxne@users.noreply.github.com>
2024-04-28 19:02:43 +0700

    Aqua.lua

--- src/Themes/Aqua.lua
+++ src/Themes/Aqua.lua
@@ -1,27 +1,27 @@
 return {
 	Name = "Aqua",
-	Accent = Color3.fromRGB(60, 165, 165),
+	Accent = Color3.fromRGB(151, 255, 255),
 
-	AcrylicMain = Color3.fromRGB(20, 20, 20),
-	AcrylicBorder = Color3.fromRGB(50, 100, 100),
+	AcrylicMain = Color3.fromRGB(198, 266, 255),
+	AcrylicBorder = Color3.fromRGB(141, 288, 288),
 	AcrylicGradient = ColorSequence.new(Color3.fromRGB(60, 140, 140), Color3.fromRGB(40, 80, 80)),
 	AcrylicNoise = 0.92,
 
-	TitleBarLine = Color3.fromRGB(60, 120, 120),
-	Tab = Color3.fromRGB(140, 180, 180),
+	TitleBarLine = Color3.fromRGB(141, 238, 238),
+	Tab = Color3.fromRGB(121, 205, 205),
 
-	Element = Color3.fromRGB(110, 160, 160),
-	ElementBorder = Color3.fromRGB(40, 70, 70),
-	InElementBorder = Color3.fromRGB(80, 110, 110),
+	Element = Color3.fromRGB(224, 238, 238),
+	ElementBorder = Color3.fromRGB(193, 205, 205),
+	InElementBorder = Color3.fromRGB(135, 206, 235),
 	ElementTransparency = 0.84,
 
 	ToggleSlider = Color3.fromRGB(110, 160, 160),
-	ToggleToggled = Color3.fromRGB(0, 0, 0),
+	ToggleToggled = Color3.fromRGB(0, 154, 205),
 
 	SliderRail = Color3.fromRGB(110, 160, 160),
 
-	DropdownFrame = Color3.fromRGB(160, 200, 200),
-	DropdownHolder = Color3.fromRGB(40, 80, 80),
+	DropdownFrame = Color3.fromRGB(126, 192, 238),
+	DropdownHolder = Color3.fromRGB(0, 191, 255),
 	DropdownBorder = Color3.fromRGB(40, 65, 65),
 	DropdownOption = Color3.fromRGB(110, 160, 160),
 
@@ -31,14 +31,14 @@ return {
 	InputFocused = Color3.fromRGB(20, 10, 30),
 	InputIndicator = Color3.fromRGB(130, 170, 170),
 
-	Dialog = Color3.fromRGB(40, 80, 80),
-	DialogHolder = Color3.fromRGB(30, 60, 60),
-	DialogHolderLine = Color3.fromRGB(25, 50, 50),
-	DialogButton = Color3.fromRGB(40, 80, 80),
-	DialogButtonBorder = Color3.fromRGB(80, 110, 110),
-	DialogBorder = Color3.fromRGB(50, 100, 100),
-	DialogInput = Color3.fromRGB(45, 90, 90),
-	DialogInputLine = Color3.fromRGB(130, 170, 170),
+	Dialog = Color3.fromRGB(191, 239, 255),
+	DialogHolder = Color3.fromRGB(188, 210, 238),
+	DialogHolderLine = Color3.fromRGB(224, 255, 255),
+	DialogButton = Color3.fromRGB(240, 255, 255),
+	DialogButtonBorder = Color3.fromRGB(173, 226, 210),
+	DialogBorder = Color3.fromRGB(0, 244, 255),
+	DialogInput = Color3.fromRGB(187, 255, 255),
+	DialogInputLine = Color3.fromRGB(127, 255, 212),
 
 	Text = Color3.fromRGB(240, 240, 240),
 	SubText = Color3.fromRGB(170, 170, 170),
